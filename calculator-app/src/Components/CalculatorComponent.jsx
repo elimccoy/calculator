2a1adb29d18a1b74d94d351a6851b6ade8e143a3
Elijah <elijmccoy01@gmail.com>
2021-10-21 14:38:24 -0700

    Addition of - button. Addition of Cos and Sin. Addition of brackets and curly brackets. Addition of decimal space.

--- calculator-app/src/Components/CalculatorComponent.jsx
+++ calculator-app/src/Components/CalculatorComponent.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Style from '../Styles/CalculatorComponent.module.css'
 import CustomButton from './CustomButton';
+import shuntingYardAlgorithm from '../Methods/ShuntingYard';
 
 const CalculatorComponent = () => 
 {
@@ -17,107 +18,94 @@ const CalculatorComponent = () =>
     s.push(buttonVal);
     setInput(s);
 
-    setVisibleString(visibleString + buttonVal);
+    if(buttonVal !== 'Neg')
+    {
+      setVisibleString(visibleString + buttonVal);
+    }
+    else
+    {
+      setVisibleString(visibleString + '-');
+    }
 
     console.log('Array: ' + input.toString());
     console.log('Visible String: ' + visibleString);
   } 
 
-  const shuntingYardAlgorithm = () => 
-  {
-    let stack = [];
-    let queue = [];
-
-    //Iterate over input and record in stack and queue.
-    for(let i = 0; i < input.length; i++)
-    {
-      let curData = input[i];
-      console.log('CurData = ' + curData);
-
-      //If this is an operator:
-      if(curData === '/' || curData === '*' || curData === '+' || curData === '-' || curData === '(' || curData === ')')
-      {
-        //Create a precedence table:
-        //let precedenceTable = [['^'], ['*', '/'], ['+', '-'], []]
-
-        console.log('stack push: ' + curData);
-        stack.push(curData);
-        console.log('Stack: ' + stack.toString());
-
-        //Check for order of operations by peeking the stack.
-        let topOfStack = stack.pop();
-        stack.push(topOfStack);
-        console.log('outofstackif');
-      }
-
-      //If this is a number:
-      if(curData === 1 || curData === 2 || curData === 3 || curData === 4 || curData === 5 || curData === 6
-        || curData === 7 || curData === 8 ||curData === 9 || curData === 0)
-      {
-
-        let fullNumber = curData;
-        console.log(fullNumber);
-        let temp = i;
-        
-        let numAdded = 0;
-        while (temp !== input.length - 1) {
-          
-          let next = input[temp + 1];
-          console.log('Next: ' + next );
-          if (next === '/' || next === '*' || next === '+' || next === '-') {
-            break;
-          }
-          else {
-            numAdded++;
-            for(let j = 0; j < numAdded; j++)
-            {
-              fullNumber = fullNumber * 10;
-            }
-            fullNumber += next;
-          }
-
-          temp++;
-        }
-
-        //Reposition pointer.
-        i = temp;
-        
-        console.log('enqueue: ' + fullNumber);
-        queue.push(fullNumber);
-        console.log('queue: ' + queue.toString());
-      }
-    }
-
-    //Pop the rest of the data off the stack and enqueue
-    while(stack.length !== 0)
-    {
-      queue.push(stack.pop());
-    }
-
-    return queue;
-  }
-
+  //postfixEvaluation
+  //Desc: Takes an input of a postfix expression and returns the result.
   const postfixEvaluation = (postfixExpression) =>
   {
+    console.log('Postfix Eval...');
     let stack = []
 
     //Iterate over expression and use stack to evaluate.
     for(let i = 0; i < postfixExpression.length; i++)
     {
       let curData = postfixExpression.at(i);
-      if(curData !== '/' && curData !== '*' && curData !== '+' && curData !== '-')
+      console.log('curData = ' + curData);
+
+      if(curData !== '/' && curData !== '*' && curData !== '+' && curData !== '-' && curData !== '^' 
+        && curData !== 'Sin(' && curData !== 'Cos(')
       {
-        stack.push(postfixExpression.at(i));
+        console.log('stack push: ' + curData);
+        stack.push(curData);
+        console.log('stack: ' + stack.toString());
       }
       else
       {
-        //Get the values that need to be evaluated.
-        let firstVal = stack.pop();
-        let secondVal = stack.pop();
-        let operator = postfixExpression.at(i);
-        let result = eval(secondVal + operator + firstVal);
 
-        stack.push(result);
+        //If trig func:
+        if(curData === 'Sin(' || curData === 'Cos(')
+        {
+          let firstVal = stack.pop();
+          let operator = curData;
+          let result = null;
+
+          switch (operator) {
+            case 'Sin(':
+              result = Math.sin(firstVal);
+              break;
+            case 'Cos(':
+              result = Math.cos(firstVal);
+              break;
+          }
+
+          stack.push(result);
+        }
+        else
+        {
+          //Get the values that need to be evaluated.
+          let firstVal = stack.pop();
+          let secondVal = stack.pop();
+          let operator = curData;
+          let result = null;
+
+          switch (operator) {
+            case '/':
+              result = secondVal / firstVal;
+              break;
+            case '*':
+              result = secondVal * firstVal;
+              break;
+            case '+':
+              result = secondVal + firstVal;
+              break;
+            case '-':
+              result = secondVal - firstVal;
+              break;
+            case '^':
+              result = secondVal;
+              for (let j = 1; j < firstVal; j++) {
+                result = result * secondVal;
+              }
+              break;
+            case 'Sin(':
+              result = Math.sin(secondVal);
+              break;
+          }
+
+          stack.push(result);
+        }
       }
     }
 
@@ -129,9 +117,11 @@ const CalculatorComponent = () =>
   // have correct synatax, the result will be an error.
   const evaluate = () =>
   {
-    let result = shuntingYardAlgorithm();
+    let result = shuntingYardAlgorithm(input);
     console.log('post shunting yard: ' + result);
     result = postfixEvaluation(result);
+    
+    //Clear input and show result.
     setInput([]);
     setVisibleString(result);
   }
@@ -145,6 +135,8 @@ const CalculatorComponent = () =>
       </div>
       <div className={Style.ButtonsSection}>
         <div className={Style.CalculatorButtonContainer}>
+          <CustomButton value = {'Sin'} onClick={()=>{ buttonClick('Sin(') }}/>
+          <CustomButton value = {'Cos'} onClick={()=>{ buttonClick('Cos(') }}/>
           <CustomButton value = {1} onClick={()=>{ buttonClick(1) }}/>
           <CustomButton value = {2} onClick={()=>{ buttonClick(2) }}/>
           <CustomButton value = {3} onClick={()=>{ buttonClick(3) }}/>
@@ -158,6 +150,12 @@ const CalculatorComponent = () =>
           <CustomButton value = {9} onClick={()=>{ buttonClick(9) }}/>
           <CustomButton value = {')'} onClick={()=>{ buttonClick(')') }}/>
           <CustomButton value = {0} onClick={()=>{ buttonClick(0) }}/>
+          <CustomButton value = {'.'} onClick={()=>{ buttonClick('.') }}/>
+          <CustomButton value = {'{'} onClick={()=>{ buttonClick('{') }}/>
+          <CustomButton value = {'}'} onClick={()=>{ buttonClick('}') }}/>
+          <CustomButton value = {'['} onClick={()=>{ buttonClick('[') }}/>
+          <CustomButton value = {']'} onClick={()=>{ buttonClick(']') }}/>
+          <CustomButton value = {'(-)'} onClick={()=>{ buttonClick('Neg') }}/>
         </div>
         <div className={Style.CalculatorOperatorContainer}>
           <CustomButton value = {'-'} onClick={()=>{ buttonClick('-') }}/>
